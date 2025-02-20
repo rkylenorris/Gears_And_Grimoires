55ed7d1ddec9fcce1b2f1747899f4079d90c0104
Kyle Norris <r.kyle.norris@gmail.com>
2025-02-20 06:22:42 -0500

    changed constructor

--- app/character_sheet/character_sheet.py
+++ app/character_sheet/character_sheet.py
@@ -15,19 +15,18 @@ class CharacterSheet:
     hit_points: HitPoints
     appearance: str
     
-    def __init__(self, name: str, player_class: CharacterClass, race: Race, background: Background,
-                 equipment: Equipment, proficencies: Proficencies, armor_class: int,
-                 initiative: int, hit_points: HitPoints, appearance: str):
+    def __init__(self, name: str, player_class: CharacterClass, race: Race,
+                 background: Background, appearance: str):
         self.name = name
         self.player_class = player_class
         self.race = race
         self.background = background
         self.abilities = AbilityScores(starting_scores=player_class.starting_ability_scores)
-        self.equipment = equipment
-        self.proficencies = proficencies
-        self.armor_class = armor_class
-        self.initiative = initiative
-        self.hit_points = hit_points
+        # self.equipment = equipment
+        # self.proficencies = proficencies
+        # self.armor_class = armor_class
+        self.hit_points = HitPoints()
+        self.hit_points.roll_hit_points(player_class.hit_die, self.abilities.endurance_core)
         self.appearance = appearance
     
     
